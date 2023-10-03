d59681277b5d3de8bf775d71b64a1b0914cf2b11
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-10-02 20:59:26 -0700

    add catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import sqlalchemy
+from src import database as db
 
 router = APIRouter()
 
@@ -10,12 +12,20 @@ def get_catalog():
     """
 
     # Can return a max of 20 items.
+    query = "SELECT num_red_potions FROM global_inventory"
+
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(query))
+    
+    row = result.fetchone()
+    if row is not None:
+        num_red_potions = row[0]
 
     return [
             {
                 "sku": "RED_POTION_0",
                 "name": "red potion",
-                "quantity": 1,
+                "quantity": num_red_potions,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
             }
