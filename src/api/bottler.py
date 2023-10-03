b6e450b94d98735b056642c29ef4db4d3fd4d694
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-10-02 20:59:13 -0700

    add bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from enum import Enum
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/bottler",
@@ -13,10 +15,29 @@ class PotionInventory(BaseModel):
     potion_type: list[int]
     quantity: int
 
+# increase red pots decrease red ml
 @router.post("/deliver")
 def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     """ """
     print(potions_delivered)
+    
+    query = "SELECT num_red_potions, num_red_ml FROM global_inventory"
+
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(query))
+
+    row = result.fetchone()
+    if row is not None:
+        pots = row[0]
+        ml = row[1]
+
+    pots += potions_delivered[0].quantity
+    ml -= potions_delivered[0].quantity * 100
+
+    query = "UPDATE global_inventory SET num_red_potions = {}, num_red_ml = {}".format(pots, ml)
+
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(query))
 
     return "OK"
 
@@ -33,9 +54,23 @@ def get_bottle_plan():
 
     # Initial logic: bottle all barrels into red potions.
 
+    query = "SELECT num_red_ml FROM global_inventory"
+
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(query))
+
+    row = result.fetchone()
+    if row is not None:
+        ml = row[0]
+
+    quantity = 0
+    while ml >= 100:
+        ml -= 100
+        quantity += 1
+    
     return [
             {
                 "potion_type": [100, 0, 0, 0],
-                "quantity": 5,
+                "quantity": quantity,
             }
         ]
