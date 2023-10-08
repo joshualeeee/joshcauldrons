ca591c00eec117b3b5a8566c0cc339535bf885cb
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-10-07 20:07:32 -0700

    catalog multipots

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -12,7 +12,7 @@ def get_catalog():
     """
 
     # Can return a max of 20 items.
-    query = "SELECT num_red_potions FROM global_inventory"
+    query = "SELECT num_red_potions, num_blue_potions, num_green_potions FROM global_inventory"
 
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(query))
@@ -20,16 +20,38 @@ def get_catalog():
     row = result.fetchone()
     if row is not None:
         num_red_potions = row[0]
+        num_blue_potions = row[1]
+        num_green_potions = row[2]
+    
+    pots = []
+    
+    
 
     if num_red_potions > 0:
-        return [
-            {
+        pots.append({
                 "sku": "RED_POTION_0",
                 "name": "red potion",
                 "quantity": num_red_potions,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
-            }
-        ]
-    else:
-        return []
+            })
+    
+    if num_blue_potions > 0:
+        pots.append({
+                "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": num_blue_potions,
+                "price": 60,
+                "potion_type": [0, 0, 100, 0],
+            })
+        
+    if num_green_potions > 0:
+        pots.append({
+                "sku": "GREEN_POTION_0",
+                "name": "green potion",
+                "quantity": num_green_potions,
+                "price": 50,
+                "potion_type": [0, 100, 0, 0],
+            })
+    
+    return pots
