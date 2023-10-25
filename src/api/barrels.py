d60bbc90cd497b59019cf19e0c3537a2c955a9b3
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-10-25 11:14:01 -0700

    barrel limit

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -84,7 +84,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
         res = []
         bought = 0
-        while gold >= 50:
+        while gold >= 50 and bought < 3:
             for barrel in wholesale_catalog:
                 if options[current_index] == barrel.potion_type and barrel.price <= gold:
                     bought += 1
